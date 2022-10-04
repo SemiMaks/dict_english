96541694e03a1503eeb5ca34e844305fc2705959
Maksim <semimaks@mail.ru>
2022-10-04 22:34:10 +0300

    Добавлены слова в словарь

--- main.py
+++ main.py
@@ -6,38 +6,43 @@ scores = 0
 
 
 def train():
-    scores = 0
-    num = 3
-    ans = 0
-    dict_k = leaters.keys()
-    print(dict_k)
+    try:
+        scores = 0
+        ans = 0
+        num = int(input('Сколько слов готов перевести?:'))
+        dict_k = leaters.keys()
+        print(dict_k)
 
-    dict_v = leaters.values()
-    print(dict_v)
-    print()
-
-    print('Приветствую тебя в языковом тренажёре!')
-    print('Требуется перевести ', num, ' слов(a).')
-    print('Что ж, приступим. Удачи!)')
-    print()
-
-    while ans != num:
-        res, let = key, val = random.choice(list(leaters.items()))
-        print('Переведи слово: ', str.capitalize(res))
-        answer = input()
-        answer = str.lower(answer)
-        if answer == str.lower(let):
-            print('Всё верно, это - ', str.lower(let))
-            ans += 1
-            scores += 1
-        else:
-            print('Не правильно!')
-            print(str.capitalize(key), ' это -', let)
-            print('Верно отгадано слов: ', scores)
-            break
-
-        print('Всего ты отгадал ', scores, 'слов(а)!')
+        dict_v = leaters.values()
+        print(dict_v)
         print()
+
+        print('Приветствую тебя в языковом тренажёре!')
+        print('Требуется перевести ', num, ' слов(a).')
+        print('Что ж, приступим. Удачи!)')
+        print()
+
+        while ans != num:
+            res, let = key, val = random.choice(list(leaters.items()))
+            print('Переведи слово: ', str.capitalize(res))
+            answer = input()
+            answer = str.lower(answer)
+            if answer == str.lower(let):
+                print('Всё верно, это - ', str.lower(let))
+                ans += 1
+                scores += 1
+            else:
+                print('Не правильно!')
+                print(str.capitalize(key), ' это -', let)
+                print('Верно отгадано слов: ', scores)
+                break
+
+            print('Всего ты отгадал ', scores, 'слов(а)!')
+            print()
+    except Exception:
+        print('Ошибка при выполнении программы!!!')
+    finally:
+        print('Работа программы успешно завершена!')
 
 
 train()
